7ac54f2a09c4cbd2024ed32539beb1bc34be62ca
Andrew Vergun <andrewvergun@gmail.com>
2025-02-07 14:12:34 +0100

    Sidebar improved

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -4,32 +4,37 @@ import OrdersIcon from '../../assets/orders_icon.svg'
 import LocationsIcon from '../../assets/map.png'
 import TypesIcon from '../../assets/list.png'
 import { Link } from 'react-router'
+import { useState } from 'react'
 
-const SideBar = (props:any) => {
+const SideBar = (props: any) => {
+  const [activeLink, setActiveLink] = useState<string>(''); // Track active button
 
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link); // Set active link when clicked
+  }
 
   return (
     <div className='sidebar'>
-        <Link to='/orders'>
-        <div className="orders">
-            <img src={OrdersIcon} alt="" />
-            <p>Items</p>
+      <Link to='/orders' onClick={() => handleLinkClick('orders')}>
+        <div className={`sidebar-item ${activeLink === 'orders' ? 'active' : ''}`}>
+          <img src={OrdersIcon} alt="Orders" />
+          <p>Items</p>
         </div>
-        </Link>
-        <Link to='/types'>
-        <div className="orders">
-            <img src={TypesIcon} alt="" />
-            <p>Types</p>
+      </Link>
+      <Link to='/types' onClick={() => handleLinkClick('types')}>
+        <div className={`sidebar-item ${activeLink === 'types' ? 'active' : ''}`}>
+          <img src={TypesIcon} alt="Types" />
+          <p>Types</p>
         </div>
-        </Link>
-        <Link to='/locations'>
-        <div className="orders">
-            <img src={LocationsIcon} alt="" />
-            <p>Locations</p>
+      </Link>
+      <Link to='/locations' onClick={() => handleLinkClick('locations')}>
+        <div className={`sidebar-item ${activeLink === 'locations' ? 'active' : ''}`}>
+          <img src={LocationsIcon} alt="Locations" />
+          <p>Locations</p>
         </div>
-        </Link>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
